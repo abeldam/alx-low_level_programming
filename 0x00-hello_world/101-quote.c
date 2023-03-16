7413e46ee2c7a30c87b7e83da106b7d8f4e357fe
Abel Damtew <abel.damtew@gmail.com>
2023-03-16 06:38:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
 #include <unistd.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 1 (Error)
- */
+*/
 int main(void)
 {
     write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
