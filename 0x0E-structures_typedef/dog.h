e7d407b4d83afa2f5f32d0b76c58b3855ea1f36a
Abel Damtew <abel.damtew@gmail.com>
2023-04-19 00:31:47 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic information
+ * struct dog - a dog's basic info
  * @name: First member
  * @age: Second member
  * @owner: Third member
@@ -15,3 +15,17 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
