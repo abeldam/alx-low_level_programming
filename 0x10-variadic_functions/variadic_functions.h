f2d9262befef54bf076574fef7bebb6c01972e08
Abel Damtew <abel.damtew@gmail.com>
2023-04-20 06:21:13 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,14 @@
-#ifndef VAR_FUNC_H
-#define VAR_FUNC_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
